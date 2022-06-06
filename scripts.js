4ee2bb03e5ee4a6d6c43f3cd67ef65b4b6680d94
tcjbeee <tcjbeee@gmail.com>
2022-06-06 16:31:33 +0100

    Added Base HTML & CSS for Calculator. Also added base Math Functions for Calculator.

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let input1;
+let input2;
+
+function add(input1, input2) {
+    let result = (input1 + input2);
+    return result;
+}
+
+function subtract(input1, input2) {
+    let result = (input1 - input2);
+    return result;
+}
+
+function multiply (input1, input2) {
+    let result = (input1 * input2);
+    return result;
+}
+
+function divide(input1, input2) {
+    let result = (input1 / input2);
+    return result;
+}
