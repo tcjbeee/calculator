867e6134b292057b851f80a3d19339a68dac8a67
tcjbeee <tcjbeee@gmail.com>
2022-06-08 10:09:34 +0100

    Finished Number Button Logic

--- scripts.js
+++ scripts.js
@@ -20,3 +20,31 @@ function divide(input1, input2) {
     let result = (input1 / input2);
     return result;
 }
+
+function operate() {
+
+}
+
+numberButtonPressed = document.querySelectorAll(".numberContainer button");
+
+currentResult = document.querySelector(".result");
+
+
+
+numberButtonPressed.forEach((button) =>
+    button.addEventListener("click", () => appendButtonPress())
+    );
+    
+
+function appendButtonPress() {
+    buttonClicked = event.target;
+
+    if (currentResult.innerHTML == 0) {
+        currentResult.innerHTML = buttonClicked.innerHTML;
+    }
+
+    else {
+    currentResult.innerHTML = (currentResult.innerHTML + buttonClicked.innerHTML);
+    }
+}
+
